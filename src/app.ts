93ca34257dfdd1b2847d2079ff1331a89e7b1724
jhaemis-hack <gbemilekekenny@gmail.com>
2025-08-21 11:56:33 +0100

    chore: added fe url to cors

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://dsa-fe.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'authorization'],
   credentials: true //sending cookies or auth headers
